46e2f11c238fe916782549e01ef8f2aa260225fd
colin <1322544157@qq.com>
2024-12-07 18:38:53 +0800

    添加FriendService:GetFriendList

---
+++ example/caller/callfriendservice.cpp
@@ -0,0 +1,32 @@
+#include "friend.pb.h"
+#include "myrpc.hpp"
+#include "rpcchannel.hpp"
+#include <iostream>
+#include <string>
+using namespace std;
+using namespace fixbug;
+
+int main(int argc, char **argv) {
+    MyRpcApplication::getInstance()->Init(argc, argv);
+
+    GetFriendListRequest request;
+    request.set_userid(0);
+    GetFriendListResponse response;
+
+    FriendServiceRpc_Stub stub(new MyRpcChannel());
+    RpcController controller;
+    stub.GetFriendList(&controller, &request, &response, nullptr);
+
+    if (controller.Failed()) {
+        cout << controller.ErrorText() << endl;
+    } else {
+        if (0 == response.result().errcode()) {
+            int size = response.friends_size();
+            for (int i = 0; i < size; ++i) {
+                cout << "index:" << (i + 1) << " name:" << response.friends(i) << endl;
+            }
+        } else {
+            cout << "rpc GetFriendsList response error : " << response.result().errmsg() << endl;
+        }
+    }
+}
